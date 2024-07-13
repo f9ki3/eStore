9e98c2cdac7048f939813ed7cbe53500ace2ca3c
Fyke Lleva <floterina@gmail.com>
2024-07-13 22:44:32 +0800

    updated cover and profile client side

--- static/js/change_cover.js
+++ static/js/change_cover.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+    var originalImageSrc = $('#bannerImage').attr('src');
+  
     $('#changecover').on('click', function() {
       $('#fileCover').click();
     });
@@ -9,9 +11,24 @@ $(document).ready(function() {
         var reader = new FileReader();
         reader.onload = function(e) {
           $('#bannerImage').attr('src', e.target.result);
+          // Disable the #changeprofile button if a file is selected
+          $('#changeprofile').attr('disabled', 'disabled');
+          // Show the #bannerValidation div
+          $('#bannerValidation').css('display', 'flex');
         };
         reader.readAsDataURL(file);
       }
+    });
+  
+    $('#cancelBanner').on('click', function() {
+      // Reset the file input
+      $('#fileCover').val('');
+      // Revert the image to the original source
+      $('#bannerImage').attr('src', originalImageSrc);
+      // Enable the #changeprofile button
+      $('#changeprofile').removeAttr('disabled');
+      // Hide the #bannerValidation div
+      $('#bannerValidation').css('display', 'none');
     });
   });
   
--- static/js/change_profile.js
+++ static/js/change_profile.js
@@ -1,4 +1,6 @@
-$(document).ready(function() {
+  $(document).ready(function() {
+    var originalImageSrc = $('#profileImage').attr('src');
+  
     $('#changeprofile').on('click', function() {
       $('#fileProfile').click();
     });
@@ -9,9 +11,25 @@ $(document).ready(function() {
         var reader = new FileReader();
         reader.onload = function(e) {
           $('#profileImage').attr('src', e.target.result);
+          // Disable the #changecover button if a file is selected
+          $('#changecover').attr('disabled', 'disabled');
+          // Show the #bannerValidation div
+          $('#profileValidation').css('display', 'flex');
         };
         reader.readAsDataURL(file);
       }
     });
+  
+    $('#cancelProfile').on('click', function() {
+      // Reset the file input
+      $('#fileProfile').val('');
+      // Revert the image to the original source
+      $('#profileImage').attr('src', originalImageSrc);
+      // Enable the #changeprofile button
+      $('#changecover').removeAttr('disabled');
+      // Hide the #bannerValidation div
+      $('#profileValidation').css('display', 'none');
+    });
   });
+  
   
